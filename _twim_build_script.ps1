b47a247d95f696bbbd63b8436e9a9484514c54f1
thatwhichis <thatwhichis@gmail.com>
2020-11-17 12:25:53 -0500

    Initial commit

---
+++ _twim_build_script.ps1
@@ -0,0 +1,12 @@
+﻿$location = Get-Location;
+$src = -join($location, '\src');
+$dst = -join($location, '\build');
+
+Get-ChildItem $src -Filter *.txt | 
+
+Foreach-Object {
+    $path = Join-Path -Path $dst -ChildPath (-join($_.BaseName, '.txt'))
+    (Get-Content $_.FullName) -join '' | Set-Content -Path $path
+}
+
+Read-Host -Prompt 'Press Enter to close'
